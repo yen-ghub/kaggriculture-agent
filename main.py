850c65a0607102a4b3fdf0f1c1def445bb78b3bf
Yendrew <yendrew.y@gmail.com>
2026-08-22 13:52:32 +1000

    perf: expand melon farm to twelve tiles

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # Define tiles to manage in (x,y)
 TILES_MANAGED = [(4,4),(3,4),(2,4),(1,4),(0,4),
                 (4,3),(3,3),(2,3),(1,3),(0,3),
-                (4,2),(3,2)]
+                (4,2),(3,2),(2,2)]
 
  # Choose crop to plant
 CROP = "MELON"

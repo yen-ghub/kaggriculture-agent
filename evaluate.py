d1141d0c5f38105ebc664c17874b3907c7b2fde2
Yendrew <yendrew.y@gmail.com>
2026-09-09 11:03:16 +1000

    feat: add fertiliize actions to hands if they have spare capacity

--- evaluate.py
+++ evaluate.py
@@ -1,13 +1,10 @@
 from kaggle_environments import make
 from main import agent, CROPS_MANAGED
-# from baselines.second_quadrant_v1 import agent as second_quadrant_agent
 from baselines.low_strawberry_test import agent as low_strawberry_agent
 # from baselines.adaptive_strawberry_sales_v1 import agent as adaptive_strawberry_sales_agent
 from baselines.endgame_liquidation_v1 import agent as endgame_liquidation_agent
 from baselines.four_sheep_v1 import agent as four_sheep_agent
 from baselines.full_second_quadrant_v1 import agent as second_quadrant_agent
-from baselines.adaptive_tomato_v1 import agent as adaptive_tomato_agent
-from baselines.adaptive_animal_v1 import agent as adaptive_animal_agent
 from baselines.third_quadrant_v1 import agent as third_quadrant_agent
 from baselines.strawberry_expansion_v1 import agent as strawberry_expansion_agent
 from baselines.hand_weed_clearing_v1 import agent as hand_weed_clearing_agent
@@ -25,19 +22,17 @@ from baselines.day8_ne_strawberry_v1 import agent as day8_ne_strawberry_agent
 from baselines.staggered_early_sheep_v1 import agent as staggered_early_sheep_agent
 from baselines.sw_livestock_reservation_v1 import agent as sw_livestock_reservation_agent
 from baselines.yarn_first_sw_sheep_v1 import agent as yarn_first_sw_sheep_agent
+from baselines.fix_hand_fertilizer_v1 import agent as fix_hand_fertilizer_v1_agent
 
 # Define variables
 SEEDS = list(range(1,21))
 # SEEDS = [38, 42, 55]
 OPPONENTS = {
-    # "second_quadrant_agent":second_quadrant_agent,
     # "low_strawberry_agent":low_strawberry_agent,
     # "adaptive_strawberry_sales_v1": adaptive_strawberry_sales_agent,
     # "endgame_liquidation_sales_v1": endgame_liquidation_agent,
     # "four_sheep_v1": four_sheep_agent,
     # "second_quadrant_v1": second_quadrant_agent,
-    # "adaptive_tomato_v1": adaptive_tomato_agent,
-    # "adaptive_animal_v1": adaptive_animal_agent,
     # "third_quadrant_v1": third_quadrant_agent,
     # "strawberry_expansion_v1": strawberry_expansion_agent,
     # "hand_weed_clearing_v1": hand_weed_clearing_agent,
@@ -53,7 +48,8 @@ OPPONENTS = {
     # "day8_ne_strawberry_v1": day8_ne_strawberry_agent,
     # "staggered_early_sheep_v1": staggered_early_sheep_agent,
     # "sw_livestock_reservation_v1": sw_livestock_reservation_agent,
-    "yarn_first_sw_sheep_v1": yarn_first_sw_sheep_agent,
+    # "yarn_first_sw_sheep_v1": yarn_first_sw_sheep_agent,
+    "fix_hand_fertilizer_v1": fix_hand_fertilizer_v1_agent,
 }
 
 PRODUCTS_TRACKED = CROPS_MANAGED + ("MILK", "WOOL", "FERTILIZER")
